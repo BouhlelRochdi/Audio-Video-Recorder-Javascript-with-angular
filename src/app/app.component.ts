743691917e09bdc2f4b437ff5ff75f5fe543137c
BouhlelRochdi <rochdi.bouhlel@hotmail.fr>
2023-02-15 17:13:16 +0100

    enhance fonctionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,102 +1,131 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  // To swith from using vanilla JS to recorderRtc library follow this link
+  // https://stackblitz.com/edit/angular-audio-recorder?file=src%2Fapp%2Fapp.component.html,src%2Fapp%2Fapp.component.ts,package.json,src%2Fapp%2Faudio-recording.service.ts
+  
   title = 'projectTest';
   @ViewChild('recordedSound') recordedSound!: ElementRef;
   @ViewChild('sound') sound!: ElementRef;
 
-  videoElement!: HTMLVideoElement;
-  recordSound!: HTMLVideoElement;
+  videoElement!: HTMLAudioElement;
+  // recordSound!: HTMLVideoElement;
+  recordSound!: HTMLAudioElement;
   mediaRecorder!: any;
-  recordedBlobs!: Blob[];
+  recordedBlobs: Blob[] = [];
   isRecording: boolean = false;
   downloadUrl!: string;
   stream!: MediaStream;
-  hidden=true;
+  hidden: boolean = true;
 
-  constructor() {}
+  // counter
+  time: number = 0;
+  display!: any;
+  interval!: any;
+
+  constructor() { }
 
   async ngOnInit() {
-    navigator.mediaDevices
-      .getUserMedia({
-        audio: true,
-        video: false
-      })
-      .then(stream => {
-        this.videoElement = this.sound.nativeElement;
-        this.recordSound = this.recordedSound.nativeElement;
-
-        this.stream = stream;
-        this.videoElement.srcObject = this.stream;
-      });
   }
 
-  startRecording() {
-    this.recordedBlobs = [];
-    let options: any = { mimeType: 'video/webm' };
+  async startRecording() {
+    this.hidden = true;
+    let options: any = { mimeType: 'audio/webm' };
 
+
+    let stream = await navigator.mediaDevices
+      .getUserMedia({
+        audio: true,
+        video: false,
+      });
+    this.videoElement = this.sound.nativeElement;
+    this.recordSound = this.recordedSound.nativeElement;
+
+    this.stream = stream;
+    this.videoElement.srcObject = this.stream;
     try {
-      this.mediaRecorder = new MediaRecorder(this.stream, options);
+      this.startTimer();
+      this.mediaRecorder = await new MediaRecorder(this.stream, options)
     } catch (err) {
       console.log(err);
     }
 
-    this.mediaRecorder.start(); // collect 100ms of data
+    this.mediaRecorder.start();
+
     this.isRecording = !this.isRecording;
-    this.onDataAvailableEvent();
-    this.onStopRecordingEvent();
+    await this.onDataAvailableEvent();
+    await this.onStopRecordingEvent();
   }
 
   stopRecording() {
     this.mediaRecorder.stop();
     this.isRecording = !this.isRecording;
-    console.log('Recorded Blobs: ', this.recordedBlobs);
+    this.hidden = false;
   }
 
   playRecording() {
     if (!this.recordedBlobs || !this.recordedBlobs.length) {
-      console.log('cannot play.');
+      alert('cannot play.');
       return;
     }
     this.recordSound.play();
   }
 
-  onDataAvailableEvent() {
+  async onDataAvailableEvent() {
     try {
       this.mediaRecorder.ondataavailable = (event: any) => {
         if (event.data && event.data.size > 0) {
           this.recordedBlobs.push(event.data);
         }
-      };
+      }
     } catch (error) {
       console.log(error);
     }
   }
 
-  onStopRecordingEvent() {
+  async onStopRecordingEvent() {
     try {
-      this.mediaRecorder.onstop = (event: Event) => {
-        const videoBuffer = new Blob(this.recordedBlobs, {
+      this.mediaRecorder.onstop = async (event: Event) => {
+        const videoBuffer = await new Blob(this.recordedBlobs, {
           // type: 'video/webm'
           type: 'audio/webm'
         });
-        this.downloadUrl = window.URL.createObjectURL(videoBuffer); // you can download with <a> tag
+        this.downloadUrl = await window.URL.createObjectURL(videoBuffer); // you can download with <a> tag
         this.recordSound.src = this.downloadUrl;
+        this.pauseTimer()
       };
     } catch (error) {
       console.log(error);
     }
   }
 
-  ngAfterViewInit(){
-    console.log('this.recordedSound: ', this.recordedSound);
-    console.log('this.recordedSound.nativeElement: ', this.recordedSound.nativeElement);
+  startTimer() {
+    this.interval = setInterval(() => {
+      if (this.time === 0) {
+        this.time++;
+      } else {
+        this.time++;
+      }
+      this.display = this.transform(this.time)
+    }, 1000);
+  }
+
+  transform(value: any): string {
+    console.log('value: ', value)
+    let minutes: any = Math.floor(value / 60);
+    value  = value - minutes * 60;
+    if(minutes <10) minutes = '0'+ minutes;
+    if(value < 10) value = '0'+ value 
+    return minutes + ':' + value;
+  }
+
+  pauseTimer() {
+    clearInterval(this.interval);
   }
 }
 
